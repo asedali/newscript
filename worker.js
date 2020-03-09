df4f58f22cc79d37ad1bf34e5440cc6df2937025
aSedali <toseyyedali@gmail.com>
2020-03-09 13:35:43 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -8,18 +8,30 @@ console.log(" starting...");
       
   const page = await browser.newPage();
   console.log("go to ncript ...");
+    
+      await page.setRequestInterception(true);
+
+	page.on('request', interceptedRequest => {
+
+		console.log( 'interceptedRequest:', interceptedRequest.url() )
+		interceptedRequest.continue();
+	});
+
+	await page.setContent(`
+	<html>
+	<body>
+		<p>Hello world</p>
+	</body>
+	</html>
+	`, { waitUntil: ['load'] });
+
       
       
-    //  page.setDefaultNavigationTimeout(0);
-      //page.setDefaultTimeout(0);
-
-
-            
-  await page.goto('https://mindmindi.blogspot.com/');
+  //await page.goto('https://mindmindi.blogspot.com/');
   console.log("on new ...");
   const innerText = await page.evaluate(() => document.querySelector('p').innerText);
   console.log(innerText);
-            await page.waitFor(100000000);
+            await page.waitFor(1000000);
             console.log("out of new ...");
 
  // await page.waitFor(1000000000);
